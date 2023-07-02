6a65092c7c2e1fb04e2691070727c1e70f43d267
andywurm <andaiyewurm@gmail.com>
2023-07-01 21:04:30 -0400

    Fixed Homepage on Mobile

--- backend/frontend/src/Components/Footer.tsx
+++ backend/frontend/src/Components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
 return(
-    <div style={{color: "gray", padding: "15px"}}>Lets Play Reviews &#xA9; 2023</div>
+    <div style={{color: "gray", padding: "15px", marginBottom: '2%'}}>Lets Play Reviews &#xA9; 2023</div>
 )
 }
 export default Footer
